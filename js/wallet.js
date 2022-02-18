40672cfaa5f66205bc49a317caf1b5ef82f55175
monir3430 <miamonir3430@gmail.com>
2022-02-18 21:38:02 +0600

    Aproximate Ok

--- js/wallet.js
+++ js/wallet.js
@@ -9,11 +9,23 @@ document.getElementById('btn-submit').addEventListener('click', function(){
    const totalExpense = document.getElementById('t-expenses');
    totalExpense.innerText = parseInt(foodValue)+parseInt(rentValue)+parseInt(clotheValue);
    
+   
 //    Income Area
    const incomeField = document.getElementById('iamount');
    const balanceField = document.getElementById('bamount');
    const incomeValue = incomeField.value;
    balanceField.innerText = parseInt(incomeValue)-parseInt(foodValue)-parseInt(rentValue)-parseInt(clotheValue);
+   
+   // Error Messaging by alert
+   if(foodValue < 0  || rentValue < 0 || clotheValue < 0 || incomeValue < 0 || isNaN(foodValue)  || isNaN(rentValue) || isNaN(clotheValue) || isNaN(incomeValue) )
+{
+    alert("value cannot be negative or String")
+    
+}
+else if(balanceField.innerText < 0){
+   alert("Income is less than expenses")
+}
+
 
 });
 
@@ -28,10 +40,9 @@ document.getElementById('sbutton').addEventListener('click', function(){
    savingAmount.innerText = parseInt(incomeFieldValue)*parseInt(savingField.value)/100;
    const balanceField = document.getElementById('bamount');
    remainingBalance.innerText = balanceField.innerText-savingAmount.innerText;
-});
 
-// try {
-//    if( value == )
-// } catch () {
-   
-// }
+   if(remainingBalance.innerText < 0 ) {
+      alert("income field value can not be less than others value")
+   }
+
+});
